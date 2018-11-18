a5846d562ad6bc331c914273071d3892416bcf84
huruji <594613537@qq.com>
2018-11-18 12:47:46 +0800

    init

--- index.js
+++ index.js
@@ -2,7 +2,18 @@ const babel = require('@babel/core');
 const t = require('@babel/types');
 
 const visitor = {
-
+  DebuggerStatement(path, state) {
+    if (state && state.opts && state.opts.debugger) {
+      path.remove();
+    }
+  },
+  ExpressionStatement(path, state) {
+    if (path.expression && path.expression.type === 'CallExpression' && path.expression.callee && path.expression.callee.name === 'alert') {
+      if (state && state.opts && state.opts.alert) {
+        path.remove();
+      }
+    }
+  }
 }
 
 module.exports = function (babel) {
